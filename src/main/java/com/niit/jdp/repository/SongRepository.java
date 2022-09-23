366aa928ac4ae42f937bcaca6f3b8fffddc9b76e
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-23 22:35:29 +0530

    completed getAll() method in SongRepository

--- src/main/java/com/niit/jdp/repository/SongRepository.java
+++ src/main/java/com/niit/jdp/repository/SongRepository.java
@@ -10,13 +10,31 @@ package com.niit.jdp.repository;
 import com.niit.jdp.model.Song;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SongRepository implements Repository<Song> {
     @Override
     public List<Song> getAll(Connection connection) throws SQLException {
-        return null;
+        String readQuery = "SELECT * FROM `jukebox`.`song`;";
+        List<Song> songsList = new ArrayList<>();
+        try (Statement statement = connection.createStatement()) {
+            ResultSet songsResultSet = statement.executeQuery(readQuery);
+            while (songsResultSet.next()) {
+                int songId = songsResultSet.getInt("song_id");
+                String songName = songsResultSet.getString("song_name");
+                String artistName = songsResultSet.getString("artist_name");
+                String genre = songsResultSet.getString("genre");
+                String duration = songsResultSet.getString("duration");
+                String songPath = songsResultSet.getString("Song path");
+                Song songs = new Song(songId, songName, artistName, genre, duration, songPath);
+                songsList.add(songs);
+            }
+        }
+        return songsList;
     }
 
     @Override
