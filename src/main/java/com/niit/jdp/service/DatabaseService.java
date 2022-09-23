ea0e81eab7499a87fe2163e28312246b69316f65
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-23 22:05:56 +0530

    completed DatabaseService.java and added methods to connect to the database

--- src/main/java/com/niit/jdp/service/DatabaseService.java
+++ src/main/java/com/niit/jdp/service/DatabaseService.java
@@ -7,5 +7,43 @@
 
 package com.niit.jdp.service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseService {
+    private static final String URL = "jdbc:mysql://localhost:3306/jukebox";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Seetha@007";
+
+    private Connection connection;
+
+    public DatabaseService() {
+        this.connection = null;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    /**
+     * It connects to the database.
+     *
+     * @return A boolean value.
+     */
+    public boolean connect() throws SQLException {
+        connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        return connection != null;
+    }
+
+    /**
+     * If the connection is not null, print "Connected to the database", otherwise print "Not connected to the database"
+     */
+    public void printConnectionStatus() {
+        if (connection != null) {
+            System.out.println("Connected to the database");
+        } else {
+            System.err.println("Not connected to the database");
+        }
+    }
 }
