15f5a3ddab113ee6c45b4272518adcac36cee6ee
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-23 21:18:34 +0530

    created SongNotFoundException.java in exception package

---
+++ src/main/java/com/niit/jdp/exception/SongNotFoundException.java
@@ -0,0 +1,11 @@
+/*
+ *Author Name : Seetha Lakshmi.G.M
+ *Date : 23-09-2022
+ *Created With : IntelliJ IDEA Community Edition
+ */
+
+
+package com.niit.jdp.exception;
+
+public class SongNotFoundException {
+}
