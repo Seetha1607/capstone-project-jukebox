2554aa18bf18848de378256438609e327812cbf7
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-23 22:07:12 +0530

    created common methods in interface Repository class as generic

--- src/main/java/com/niit/jdp/repository/Repository.java
+++ src/main/java/com/niit/jdp/repository/Repository.java
@@ -1,4 +1,13 @@
 package com.niit.jdp.repository;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 public interface Repository<T> {
+    List<T> getAll(Connection connection) throws SQLException;
+
+    T getById(Connection connection, int id) throws SQLException;
+
+    boolean deleteById(Connection connection, int id) throws SQLException;
 }
