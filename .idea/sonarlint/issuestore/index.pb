73b45d3e09fa6b0efeeffaea46133cb9cf64a985
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-23 23:11:04 +0530

    completed getAll() method

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -6,15 +6,9 @@ T
 $src/main/java/com/niit/jdp/Main.java,2\e\2e972e9c43469b67c52d4ebb1ef4baf6e570e05a
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
-i
-9src/main/java/com/niit/jdp/repository/SongRepository.java,e\d\edabe8e0dc5366eb0790ca0232dca645cdbc090d
-m
-=src/main/java/com/niit/jdp/repository/PlaylistRepository.java,2\4\24809a1184a7964e7f32ebdf585553dac4af9abb
 o
 ?src/main/java/com/niit/jdp/exception/SongNotFoundException.java,5\8\58271fa51d1458ca4a9291ff7fff818f7a443c36
 s
 Csrc/main/java/com/niit/jdp/exception/PlaylistNotFoundException.java,0\4\049d9426c1be695701e297ddaa5b38eb74bd63f5
 c
 3src/main/java/com/niit/jdp/service/SongService.java,2\f\2fecdc9ffaf726830e87bbac657c9dc350f67bb5
-^
-.src/main/java/com/niit/jdp/model/Playlist.java,5\b\5babf34b02434545493a4e3c8d45ca106f1f5e26
--- src/main/java/com/niit/jdp/repository/PlaylistRepository.java
+++ src/main/java/com/niit/jdp/repository/PlaylistRepository.java
@@ -7,16 +7,33 @@
 
 package com.niit.jdp.repository;
 
+
 import com.niit.jdp.model.Playlist;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
-public class PlaylistRepository implements Repository<Playlist>{
+public class PlaylistRepository implements Repository<Playlist> {
     @Override
     public List<Playlist> getAll(Connection connection) throws SQLException {
-        return null;
+        String readQuery = "SELECT * FROM `jukebox`.`playlist`;";
+        ResultSet resultSet;
+        try (Statement statement = connection.createStatement()) {
+            resultSet = statement.executeQuery(readQuery);
+            while (resultSet.next()) {
+                System.out.format("%s     \n%s     \n%s     \n%s          \n%s\n",
+                        "Playlist ID :" + resultSet.getInt(1) + " ",
+                        "Playlist Name :" + resultSet.getString(2) + " ",
+                        "Song ID :" + resultSet.getInt(3) + " ",
+                        "Song Name :" + resultSet.getString(4) + " ",
+                        "Song Path :" + resultSet.getString(5));
+                System.out.println();
+            }
+        }
+        return List.of(new Playlist());
     }
 
     @Override
