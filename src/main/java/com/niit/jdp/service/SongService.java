71c030bee22467cfa3af8339e60192db8bb294a0
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-25 00:17:07 +0530

    created getSortedNameByOrder() method in SongService.java and used comparator() and lambda expression to complete it

--- src/main/java/com/niit/jdp/service/SongService.java
+++ src/main/java/com/niit/jdp/service/SongService.java
@@ -7,5 +7,19 @@
 
 package com.niit.jdp.service;
 
+import com.niit.jdp.model.Song;
+
+import java.util.Comparator;
+import java.util.List;
+
 public class SongService {
+    public List<Song> getSortedNameByOrder(List<Song> songName) {
+
+        Comparator<Song> nameComparator = (((o1, o2) -> String.CASE_INSENSITIVE_ORDER.compare(o1.getSongName(), o2.getSongName())));
+        songName.sort(nameComparator);
+        for (Song songs : songName)
+            songs.getSongName();
+        return songName;
+    }
+
 }
