3bd9f98af0108c12c0672c891c4e9ba47d4c58bb
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-25 02:06:04 +0530

    created and executed test cases successfully in SongServiceTest.java

--- src/test/java/com/niit/jdp/service/SongServiceTest.java
+++ src/test/java/com/niit/jdp/service/SongServiceTest.java
@@ -1,22 +1,48 @@
 package com.niit.jdp.service;
 
+import com.niit.jdp.model.Song;
+import com.niit.jdp.repository.SongRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 class SongServiceTest {
+    SongService service;
+    SongRepository songRepository;
+    DatabaseService databaseService = new DatabaseService();
 
     @BeforeEach
     void setUp() {
+        service = new SongService();
+        songRepository = new SongRepository();
     }
 
     @AfterEach
     void tearDown() {
+        service = null;
+        songRepository = null;
     }
 
     @Test
-    void getSortedNameByOrder() {
+    void getSortedNameByOrderSuccess() throws SQLException, ClassNotFoundException {
+        databaseService.connect();
+        Connection connection1 = databaseService.getConnection();
+        List<Song> all = songRepository.getAll(connection1);
+        List<Song> expectedOutput = service.getSortedNameByOrder(all);
+        Assertions.assertEquals(expectedOutput, service.getSortedNameByOrder(all));
+    }
+
+    @Test
+    void getSortedNameByOrderFailure() throws SQLException, ClassNotFoundException {
+        databaseService.connect();
+        Connection connection1 = databaseService.getConnection();
+        List<Song> all = songRepository.getAll(connection1);
+        List<Song> expectedOutput = service.getSortedNameByOrder(all);
+        Assertions.assertNotEquals(expectedOutput, service.getSortedNameByOrder(all).toString());
     }
 }
