47806975611badefdc495ca731ef8f39a359933d
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-25 00:27:49 +0530

    created Catalogue menu to display to the user

--- src/main/java/com/niit/jdp/Main.java
+++ src/main/java/com/niit/jdp/Main.java
@@ -2,6 +2,18 @@ package com.niit.jdp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int choice = 0;
+
+        do {
+            System.out.println("Welcome to the JukeBox Catalogue");
+            System.out.println("============================================");
+            System.out.println("1. View all playlist");
+            System.out.println("2. Search full song details by playlist ID");
+            System.out.println("3: Play song from the Playlist");
+            System.out.println("4. Create you're own playlist");
+            System.out.println("5. Exit");
+            System.out.println("============================================");
+
+        } while (choice != 5);
     }
 }
