d885421193276846fc9ebf4e8ffc48438fa5c67d
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-24 23:34:11 +0530

    implemented add() method in SongRepository.java and completed the logic to create new playlist

--- src/main/java/com/niit/jdp/repository/SongRepository.java
+++ src/main/java/com/niit/jdp/repository/SongRepository.java
@@ -14,6 +14,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SongRepository implements Repository<Song> {
+    /**
+     * This function is used to read all the songs from the database and return them as a list of songs
+     *
+     * @param connection This is the connection to the database.
+     * @return A list of songs.
+     */
     @Override
     public List<Song> getAll(Connection connection) throws SQLException {
         String readQuery = "SELECT * FROM `jukebox`.`song`;";
@@ -34,6 +40,34 @@ public class SongRepository implements Repository<Song> {
         return songsList;
     }
 
+    /**
+     * It takes a connection and a song object as parameters, and returns a boolean value
+     *
+     * @param connection The connection to the database.
+     * @param song       The song object that you want to add to the database.
+     * @return The number of rows affected by the query.
+     */
+    @Override
+    public boolean add(Connection connection, Song song) throws SQLException {
+        String insertQuery = "INSERT INTO `jukebox`.`song` (song_name, artist_name,genre,Song_path) VALUES (?,?,?);";
+        int numberOfRowsAffected;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+            preparedStatement.setString(1, song.getSongName());
+            preparedStatement.setString(2, song.getArtistName());
+            preparedStatement.setString(3, song.getGenre());
+            preparedStatement.setString(4, song.getSongPath());
+            numberOfRowsAffected = preparedStatement.executeUpdate();
+        }
+        return numberOfRowsAffected > 0;
+    }
+
+    /**
+     * This function is used to get a song by its id
+     *
+     * @param connection The connection to the database.
+     * @param id         The id of the song you want to get.
+     * @return A song object.
+     */
     @Override
     public Song getById(Connection connection, int id) throws SQLException {
 
@@ -55,6 +89,13 @@ public class SongRepository implements Repository<Song> {
         return songs;
     }
 
+    /**
+     * > This function deletes a song from the database by its id
+     *
+     * @param connection The connection to the database.
+     * @param id         The id of the song to be deleted.
+     * @return A boolean value.
+     */
     @Override
     public boolean deleteById(Connection connection, int id) throws SQLException {
         String deleteQuery = "DELETE FROM `jukebox`.`song` WHERE (`song_id` = ?);";
@@ -66,6 +107,13 @@ public class SongRepository implements Repository<Song> {
         return numberOfRowsAffected > 0;
     }
 
+    /**
+     * It takes a song object and inserts it into the database
+     *
+     * @param connection The connection to the database.
+     * @param song       The song object that we want to add to the database.
+     * @return The number of rows affected by the query.
+     */
     public boolean addSong(Connection connection, Song song) throws SQLException {
         String insertQuery = "INSERT INTO `jukebox`.`song` (`song_name`, `duration`, `artist_name`, `genre`, `Song path`) VALUES (?, ?, ?, ?, ?);";
         int numberOfRowsAffected;
