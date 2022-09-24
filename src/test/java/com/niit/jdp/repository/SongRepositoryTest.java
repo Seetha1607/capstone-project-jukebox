736908303bbba61c891e221fe845ebebe21e34e4
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-25 02:01:28 +0530

    created SongRepositoryTest.java

---
+++ src/test/java/com/niit/jdp/repository/SongRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.niit.jdp.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SongRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getById() {
+    }
+}
