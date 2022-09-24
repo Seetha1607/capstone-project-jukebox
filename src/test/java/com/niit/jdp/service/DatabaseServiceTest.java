254aff93108a19b2e1e62b8e49a0fdcb3c077f3c
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-25 02:03:30 +0530

    created DatabaseServiceTest.java

---
+++ src/test/java/com/niit/jdp/service/DatabaseServiceTest.java
@@ -0,0 +1,22 @@
+package com.niit.jdp.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void connect() {
+    }
+}
