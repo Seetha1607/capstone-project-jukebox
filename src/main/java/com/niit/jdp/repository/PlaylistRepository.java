27fb89cbaf7e13ca05a82f927fa9f005bc036f76
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-24 22:54:37 +0530

    implemented add() method in PlaylistRepository.java and completed the logic to create new playlist

--- src/main/java/com/niit/jdp/repository/PlaylistRepository.java
+++ src/main/java/com/niit/jdp/repository/PlaylistRepository.java
@@ -14,6 +14,12 @@ import java.sql.*;
 import java.util.List;
 
 public class PlaylistRepository implements Repository<Playlist> {
+    /**
+     * This function is used to read all the data from the database and display it in the console
+     *
+     * @param connection This is the connection to the database.
+     * @return A list of Playlist objects.
+     */
     @Override
     public List<Playlist> getAll(Connection connection) throws SQLException {
         String readQuery = "SELECT * FROM `jukebox`.`playlist`;";
@@ -21,7 +27,7 @@ public class PlaylistRepository implements Repository<Playlist> {
         try (Statement statement = connection.createStatement()) {
             resultSet = statement.executeQuery(readQuery);
             while (resultSet.next()) {
-                System.out.format("%s     \n%s     \n%s     \n%s          \n%s\n",
+                System.out.format("%s     \n%s     \n%s     \n%s\n",
                         "Playlist ID :" + resultSet.getInt(1) + " ",
                         "Playlist Name :" + resultSet.getString(2) + " ",
                         "Song ID :" + resultSet.getInt(3) + " ",
@@ -33,10 +39,31 @@ public class PlaylistRepository implements Repository<Playlist> {
     }
 
     @Override
+    public boolean add(Connection connection, Playlist playlist) throws SQLException {
+        String insertQuery = "INSERT INTO `jukebox`.`playlist` (playlist_name, song_id,song_name) VALUES ('Favo','12','Unstoppable');";
+        int numberOfRowsAffected;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+            preparedStatement.setInt(1, playlist.getPlaylistId());
+            preparedStatement.setString(2, playlist.getPlaylistName());
+            preparedStatement.setInt(3, playlist.getSongId());
+            preparedStatement.setString(4,playlist.getSongName());
+            numberOfRowsAffected = preparedStatement.executeUpdate();
+        }
+        return numberOfRowsAffected > 0;
+    }
+
+    @Override
     public Playlist getById(Connection connection, int id) throws SQLException {
         return null;
     }
 
+    /**
+     * > This function deletes a playlist from the database by its id
+     *
+     * @param connection The connection to the database.
+     * @param id The id of the playlist to delete.
+     * @return A boolean value.
+     */
     @Override
     public boolean deleteById(Connection connection, int id) throws SQLException {
         String deleteQuery = "DELETE FROM `jukebox`.`playlist` WHERE (`playlist_id` = ?);";
