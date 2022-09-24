3a4e3372102ac43a1c6a36981b931f626642c020
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-25 02:04:44 +0530

    completed and executed test cases successfully

--- src/test/java/com/niit/jdp/service/DatabaseServiceTest.java
+++ src/test/java/com/niit/jdp/service/DatabaseServiceTest.java
@@ -1,22 +1,37 @@
 package com.niit.jdp.service;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 class DatabaseServiceTest {
+    DatabaseService databaseService;
 
     @BeforeEach
     void setUp() {
+        databaseService = new DatabaseService();
     }
 
     @AfterEach
     void tearDown() {
+        databaseService = null;
     }
 
     @Test
-    void connect() {
+    void getConnectionSuccess() throws SQLException, ClassNotFoundException {
+        boolean actualOutput = databaseService.connect();
+        boolean expectedOutput = databaseService.connect();
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void getConnectionFailure() throws SQLException, ClassNotFoundException {
+        Connection actualOutput = databaseService.getConnection();
+        boolean expectedOutput = databaseService.connect();
+        Assertions.assertNotEquals(expectedOutput, actualOutput);
     }
 }
